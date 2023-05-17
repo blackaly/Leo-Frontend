2e89992e33822e399cfe30f84352e96b1fa24bfd
blackaly <algeserone@yandex.com>
2023-05-17 13:19:35 +0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutUsComponent },
-  { path: 'portfolio', component: PortfolioComponent },
+  /*{ path: 'portfolio', component: PortfolioComponent },*/
   { path: 'contact', component: ContactComponent },
   { path: 'customer', component: CustomerComponent },
 ];
